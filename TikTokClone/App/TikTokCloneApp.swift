3cdcd2bf4143ca13a27f379235e1f567d221f438
Kriti Agarwal <kritiagr13@gmail.com>
2024-01-26 17:28:10 +0000

    initial commit

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Kriti Agarwal on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
