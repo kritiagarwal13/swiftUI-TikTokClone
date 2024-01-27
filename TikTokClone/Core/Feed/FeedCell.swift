64fd32f864efcb708502f3306b009e754c3fafd8
Kriti Agarwal <kritiagr13@gmail.com>
2024-01-27 18:45:43 +0000

    Feed view

---
+++ TikTokClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,119 @@
+//
+//  FeedCell.swift
+//  TikTokClone
+//
+//  Created by Kriti Agarwal on 27/01/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var post: Int
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.pink)
+                .containerRelativeFrame([.horizontal, .vertical])
+                .overlay {
+                    Text("Post \(post)")
+                        .foregroundStyle(.white)
+                }
+            
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Carlos.saiz")
+                            .fontWeight(.semibold)
+                        
+                        Text("Rocket ship prepare for takeoff!!!")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.white)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "bookmark.fill")
+                                    .resizable()
+                                    .frame(width: 22, height: 28)
+                                .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "arrowshape.turn.up.right.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                    }
+                    
+                }
+                .padding(.bottom, 80)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FeedCell(post: 2)
+}
