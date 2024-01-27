b53cefcf79d97d0bcd4c8014f6665b00728dfd1f
Kriti Agarwal <kritiagr13@gmail.com>
2024-01-27 19:12:04 +0000

    explore view

---
+++ TikTokClone/Core/Search/UserCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserCell.swift
+//  TikTokClone
+//
+//  Created by Kriti Agarwal on 27/01/24.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(Color(.systemGray5))
+            
+            VStack(alignment: .leading) {
+                Text("test_user")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Test Name")
+                    .font(.footnote)
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    UserCell()
+}
